1769c69a914f0fc75820d65d6f67ed19d770654e
cleophas.mashiri <cleophas.mashiri@standardbank.co.za>
2016-09-21 20:23:17 +0200

    first commit

---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('app11', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ui.router', 'ui.bootstrap', 'toastr']);
+
+})();
